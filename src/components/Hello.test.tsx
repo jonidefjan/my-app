86117e55754d7630d35188d552b5be5416b1d7d2
jonidefjan <jonidefjan@gmail.com>
2019-08-29 18:13:45 -0300

    0.03

--- src/components/Hello.test.tsx
+++ src/components/Hello.test.tsx
@@ -12,6 +12,11 @@ it('renderiza o texto correto quando 1 nível de entusiasmo é dado', () =>{
     expect(hello.find(".saudacao").text()).toEqual('Eai Jonatas Mattos!')
 });
 
+it('renderiza o texto correto quando 5 nível de entusiasmo é dado', () =>{
+    const hello = enzyme.shallow( <Hello nome='Jonatas Mattos' nivelDeEntusiasmo={5} /> );
+    expect(hello.find(".saudacao").text()).toEqual('Eai Jonatas Mattos!!!!!')
+});
+
 it('lança quando o nível de entusiasmo é 0', () =>{
     expect(() => {
         enzyme.shallow(<Hello nome='Jonatas Mattos' nivelDeEntusiasmo={0} />);
