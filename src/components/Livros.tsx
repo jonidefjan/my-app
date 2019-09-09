763616de00837cd8c0d7d32c444b21cca2a3850e
jonatas mattos <jonidefjan@gmail.com>
2019-09-09 03:04:54 -0300

    ,3

--- src/components/Livros.tsx
+++ src/components/Livros.tsx
@@ -1,22 +1,15 @@
 import * as React from 'react'
-import { livrosData } from "./LivrosStore";
 
-export default class Carrinho extends React.Component{
-    renderFilters = () => {
-        return [...livrosData.filters].map(([key, value]) => {
-          return (
-            <li key={key + value}>
-              {key} - {value.toString()}
-            </li>
-          );
-        });
-      };
 
-    render(){
-        return(
-            <div>
-                
-            </div>
+export default class Carrinho extends React.Component {
+    
+
+    render() {
+       
+        return (
+            <div>livros</div>
         )
     }
 }
+
+
